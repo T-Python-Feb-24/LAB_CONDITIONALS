d66aec0e20b3557bf3a6de5fe97032fccbc0cfe9
rahaf <rahaf09876509@gmail.com>
2024-02-07 11:19:20 +0300

    lab3

---
+++ lab3.py
@@ -0,0 +1,13 @@
+moviename:str="fire"
+rating:int=3
+popularity:float=72.65
+if rating >=4 and popularity >80 :
+    print("Highly recommended")
+elif rating >=3 and popularity >70 :
+    print("I recommended it it is good")
+elif rating <=2 and popularity >60 :
+    print ("You should check it out!")
+elif rating <=2 and popularity >50:
+    print("Dont watch it.it is a waset of time")
+else:
+    print("f")
