44dc19afecdc1f17030681c281a52a97fd7547f0
Hammam <hajeddawi@gmail.com>
2024-02-07 11:08:58 +0300

    solved

--- BMI.py
+++ BMI.py
@@ -1,11 +1,11 @@
 #Ask the user to provide his wieght
-user_weight:str = float(input("Please Enter Your Weight: "))
+user_weight:str = float(input("Please enter your weight in kilograms: "))
 
 #Ask the user to provide his height
-user_height:str = float(input("Please enter your height"))
+user_height:str = float(input("Please enter your height in meters: "))
 
 # Calculate the BMI
-BMI = user_weight / (user_height ** 2)
+BMI = user_weight / (user_height**2)
 print("Your BMI is: ", BMI)
 
 # Categorize the BMI value
@@ -14,7 +14,7 @@ if BMI > 0:
         print("You are underweight. Watch your health")
     elif BMI <= 25:
         print("You are fit & healthy")
-    elif BMI <= 30:
+    elif BMI > 25:
         print("You are overwieght you need to work out more and watch your diet")
 else:
     print("ERROR. Enter valid details!!")
