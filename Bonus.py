f197f335897d3d46428621ac3c5314a5d4296b6a
Maha <mahaalqarni.offficial@gmail.com>
2024-02-07 12:05:35 +0300

    Last Commit

--- Bonus.py
+++ Bonus.py
@@ -1,14 +1,17 @@
 # write a program that Calculates the BMI 
 #(body mass index) of the user:
 
-weight = input("Please enter your weight : ")
-height = input("Please enter your height : ")
+weight = float(input("Please enter your weight : "))
+
+height = float(input("Please enter your height : "))
 
 BMI = weight/height**2
 
-if BMI <= 29.9:
-    print("You are overwieght you need to work out more and watch your diet.")
+if BMI <= 18.5:
+    print("You are underweight. Watch your health.")
 elif BMI <= 24.9:
     print("You are fit & healthy.")
-elif BMI <= 18.5:
-    print("You are underweight. Watch your health.")
+elif BMI <= 29.9:
+    print("You are overwieght you need to work out more and watch your diet.")
+
+print(BMI)
--- LAB_CONDITIONALS.py
+++ LAB_CONDITIONALS.py
@@ -32,11 +32,11 @@ and the popularity is less than 50
 '''
 if rating >= 4 and popularity_score > 80:
     print("Highly recommended")
-elif rating >= 3 and popularity_score > 70:
+elif rating >= 3 or popularity_score > 70:
     print("I recommended it . It is good ")
-elif rating <= 2 and popularity_score > 60 :
+elif rating <= 2 or popularity_score > 60 :
     print("You should check it out!")
-elif rating <=2 and popularity_score < 50:
+elif rating <=2 or popularity_score < 50:
     print("Don't watch it. It is a waste of time")
 
 
