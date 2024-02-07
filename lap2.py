c7c798df1dc402a896d14296c8164076c34e966a
Fai <faiabdulla123@gmail.com>
2024-02-07 11:39:08 +0300

    lab2

---
+++ lap2.py
@@ -0,0 +1,11 @@
+Moive="Titanic"
+Rat:int=3
+popularity: float=72.65
+if  Rat >=4 and popularity <80:
+     print("Higly recommended")
+elif Rat >=3 or popularity >70:
+    print("I recommended it . It is good")
+elif Rat <=2 or popularity >60:
+    print("You should check it out!")
+elif Rat >=2 or popularity <50:
+    print("Dont watch it. It is a waste of time")
