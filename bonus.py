9d98aed9412be0dc0f678b0993487bfba4a39996
Salemhu <salem.hussain.alsalem@gmail.com>
2024-02-07 15:06:14 +0300

    added "and" in the if statememnt.

--- bonus.py
+++ bonus.py
@@ -1,12 +1,14 @@
 weight:float = float(input("Enter your wieght in kilos : "))
 height:int = int(input("Enter your height in centimeter : "))
 BMI:float = weight / (height/100)**2
+print(round(BMI,2))
 
+if BMI >= 25:
+    print("You are overwieght you need to work out more and watch your diet.")
+
+if BMI >= 18.5 and BMI < 25:
+    print("You are fit & healthy.")
+    
 if BMI < 18.5:
     print("You are underweight. Watch your health.")
 
-elif BMI >= 18.5:
-    print("You are fit & healthy.")
-
-elif BMI >= 25:
-    print("You are overwieght you need to work out more and watch your diet.")
