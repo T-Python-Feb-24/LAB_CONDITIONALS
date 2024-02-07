cf0aeaf30f7b420fb52d83e0246196484913d101
AsmaaFelemban <Asmaakf@hotmail.com>
2024-02-07 11:25:37 +0300

    Solved lab conditions and the bonus

--- lab_conditions.py
+++ lab_conditions.py
@@ -8,7 +8,7 @@ if movie_rating >= 4 and popularity_score > 80:
     print("Highly recommended")
 elif movie_rating >= 3 and popularity_score > 70:
     print("I recommended it . It is good")
-elif movie_rating <= 2 and popularity_score < 60: 
+else: 
     print("Don't watch it. It is a waste of time") 
 
 
